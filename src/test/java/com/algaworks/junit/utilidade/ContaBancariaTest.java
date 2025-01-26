d551b7dcd96b249b0c9fcd2cd8b689bade2de476
David Alves Dutra <dadutra@hotmail.com>
2025-01-26 11:02:47 -0300

    ContaBancariaTest ok.

---
+++ src/test/java/com/algaworks/junit/utilidade/ContaBancariaTest.java
@@ -0,0 +1,112 @@
+package com.algaworks.junit.utilidade;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import java.math.BigDecimal;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class ContaBancariaTest {
+
+
+    @Test
+    void contaConstructorShouldReturnExceptioWhenSaldoIsNull() {
+        Assertions.assertThrows(IllegalArgumentException.class,
+                ()-> {
+                    ContaBancaria contaBancaria = new ContaBancaria(null);
+                });
+    }
+
+    @Test
+    void contaConstructorShouldEffectiveDepositWhenValorIsValid() {
+        ContaBancaria contaBancaria = new ContaBancaria(new BigDecimal(10));
+        Assertions.assertEquals(new BigDecimal(10), contaBancaria.saldo());
+    }
+
+    @Test
+    void saqueLançaIllegalArgumentExceptionParaValorZero() {
+        Assertions.assertThrows(IllegalArgumentException.class,
+                ()-> {
+                    ContaBancaria contaBancaria = new ContaBancaria(new BigDecimal(10));
+                    contaBancaria.saque(new BigDecimal(0));
+                });
+    }
+
+    @Test
+    void saqueLançaIllegalArgumentExceptionParaValorNulo() {
+        Assertions.assertThrows(IllegalArgumentException.class,
+                ()-> {
+                    ContaBancaria contaBancaria = new ContaBancaria(new BigDecimal(10));
+                    contaBancaria.saque(null);
+                });
+    }
+
+    @Test
+    void saqueLançaIllegalArgumentExceptionParaValorNegativo() {
+        Assertions.assertThrows(IllegalArgumentException.class,
+                ()-> {
+                    ContaBancaria contaBancaria = new ContaBancaria(new BigDecimal(10));
+                    contaBancaria.saque(new BigDecimal(-2));
+                });
+    }
+
+    @Test
+    void saqueLançaRuntimeExceptionParaSaldoInsuficiente() {
+        Assertions.assertThrows(RuntimeException.class,
+                ()-> {
+                    ContaBancaria contaBancaria = new ContaBancaria(new BigDecimal(10));
+                    contaBancaria.saque(new BigDecimal(20));
+                });
+    }
+
+    @Test
+    void saqueDebitaDoSaldoParaValorVálido() {
+        ContaBancaria contaBancaria = new ContaBancaria(new BigDecimal(10));
+        contaBancaria.saque(new BigDecimal(5));
+
+        Assertions.assertEquals(new BigDecimal(5), contaBancaria.saldo());
+    }
+
+    @Test
+    void depositoDeveLançarIllegalArgumentExceptionParaValorNulo() {
+        Assertions.assertThrows(IllegalArgumentException.class,
+                ()-> {
+                    ContaBancaria contaBancaria = new ContaBancaria(new BigDecimal(10));
+                    contaBancaria.saque(null);
+                });
+    }
+
+    @Test
+    void depositoDeveLançarIllegalArgumentExceptionParaValorNegativo() {
+        Assertions.assertThrows(IllegalArgumentException.class,
+                ()-> {
+                    ContaBancaria contaBancaria = new ContaBancaria(new BigDecimal(10));
+                    contaBancaria.saque(new BigDecimal(-10));
+                });
+    }
+
+    @Test
+    void depositoDeveLançarIllegalArgumentExceptionParaValorZero() {
+        Assertions.assertThrows(IllegalArgumentException.class,
+                ()-> {
+                    ContaBancaria contaBancaria = new ContaBancaria(new BigDecimal(10));
+                    contaBancaria.saque(new BigDecimal(0));
+                });
+    }
+
+    @Test
+    void depositoDeveAtualizarSaldoParaDepositoValido() {
+        ContaBancaria contaBancaria = new ContaBancaria(new BigDecimal(10));
+        contaBancaria.deposito(new BigDecimal(5));
+
+        Assertions.assertEquals(new BigDecimal(15), contaBancaria.saldo());
+    }
+
+    @Test
+    void saldoDeveRetornarSaldoPresente() {
+        ContaBancaria contaBancaria = new ContaBancaria(new BigDecimal(10));
+
+        Assertions.assertEquals(new BigDecimal(10), contaBancaria.saldo());
+    }
+}
