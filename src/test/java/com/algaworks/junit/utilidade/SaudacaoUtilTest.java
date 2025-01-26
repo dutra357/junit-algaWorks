ebd65cf1b206a9d25905537bc8c5eaa553fa5ca7
David Alves Dutra <dadutra@hotmail.com>
2025-01-26 10:08:34 -0300

    saudacaoUtil testes ok.

--- src/test/java/com/algaworks/junit/utilidade/SaudacaoUtilTest.java
+++ src/test/java/com/algaworks/junit/utilidade/SaudacaoUtilTest.java
@@ -1,12 +1,15 @@
 package com.algaworks.junit.utilidade;
 
 import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Assumptions;
+import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.condition.EnabledIfEnvironmentVariable;
 
 class SaudacaoUtilTest {
 
     @Test
-    public void saudarTest() {
+    public void saudarTestManha() {
         String saudacao = SaudacaoUtil.saudar(9);
         System.out.println(saudacao);
 
@@ -27,6 +30,61 @@ class SaudacaoUtilTest {
 
     @Test
     public void saudarTestShouldNotThrowException() {
+
+        Assertions.assertDoesNotThrow(() -> SaudacaoUtil.saudar(5));
+    }
+
+    @Test
+    public void saudarTestTarde() {
+        String saudacao = SaudacaoUtil.saudar(12);
+        System.out.println(saudacao);
+
+        Assertions.assertEquals("Boa tarde", saudacao, "Saudação incorreta!");
+        Assertions.assertTrue(saudacao.equals("Boa tarde"));
+    }
+
+    @Test
+    public void saudarTestNoite() {
+        String saudacao = SaudacaoUtil.saudar(22);
+        System.out.println(saudacao);
+
+        Assertions.assertEquals("Boa noite", saudacao, "Saudação incorreta!");
+        Assertions.assertTrue(saudacao.equals("Boa noite"));
+    }
+
+    @Test
+    public void saudarTestAposCincoComBomDia() {
+        String saudacao = SaudacaoUtil.saudar(5);
+        System.out.println(saudacao);
+
+        Assertions.assertEquals("Bom dia", saudacao, "Saudação incorreta!");
+        Assertions.assertTrue(saudacao.equals("Bom dia"));
+    }
+
+    @Test
+    public void saudarTestAntesDasCincoComBomDia() {
+        String saudacao = SaudacaoUtil.saudar(3);
+        System.out.println(saudacao);
+
+        Assertions.assertEquals("Boa noite", saudacao, "Saudação incorreta!");
+        Assertions.assertTrue(saudacao.equals("Boa noite"));
+    }
+
+
+
+    @Test
+    @Disabled("Repetido para teste - Teste não realziado")
+    public void saudarTestDesabilitado() {
+
+        Assertions.assertDoesNotThrow(() -> SaudacaoUtil.saudar(5));
+    }
+
+    @Test
+    //@EnabledIfEnvironmentVariable(named = "ENV", matches = "DEV")
+    //Repetido para teste, desabilitado por variavel de ambiente
+    public void saudarTestDesabilitadoPorVariavelDeAmbiente() {
+        //Desabilitado por variável ou Assumptions
+        Assumptions.assumeTrue("DEV".equals(System.getenv("ENV")), () -> "Abortando teste.");
 
         Assertions.assertDoesNotThrow(() -> SaudacaoUtil.saudar(5));
     }
