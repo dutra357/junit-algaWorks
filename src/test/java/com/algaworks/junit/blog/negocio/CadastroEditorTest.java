9544dfc3b13802ec4c8fa546eed2c1cd73cdaa41
David Alves Dutra <dadutra@hotmail.com>
2025-01-28 08:04:19 -0300

    Simulando classes com stub/duble.

--- src/test/java/com/algaworks/junit/blog/negocio/CadastroEditorTest.java
+++ src/test/java/com/algaworks/junit/blog/negocio/CadastroEditorTest.java
@@ -1,31 +1,32 @@
 package com.algaworks.junit.blog.negocio;
 
+import com.algaworks.junit.blog.exception.RegraNegocioException;
 import com.algaworks.junit.blog.modelo.Editor;
+import com.algaworks.junit.blog.negocio.stub.ArmazenamentoEditorFixoEmMemoria;
 import org.junit.jupiter.api.*;
 
 import java.math.BigDecimal;
-
-import static org.junit.jupiter.api.Assertions.*;
 
 @DisplayNameGeneration(DisplayNameGenerator.ReplaceUnderscores.class)
 class CadastroEditorTest {
 
     static CadastroEditor cadastroEditor;
     private Editor editor;
-
-    @BeforeAll
-    static void beforeAll() {
-        cadastroEditor = new CadastroEditor(new ArmazenamentoEditorFixoEmMemoria(),
-                new GerenciadorEnvioEmail() {
-                @Override
-                    public void enviarEmail(Mensagem mensagem) {
-                    System.out.println("Enviando mensagem para: " + mensagem.getDestinatario());
-                }
-                });
-    }
+    private static ArmazenamentoEditorFixoEmMemoria armazenamentoEditor;
 
     @BeforeEach
     void beforeEach() {
+        armazenamentoEditor = new ArmazenamentoEditorFixoEmMemoria();
+
+        cadastroEditor = new CadastroEditor(
+                armazenamentoEditor,
+                new GerenciadorEnvioEmail() {
+                    @Override
+                    public void enviarEmail(Mensagem mensagem) {
+                        System.out.println("Enviando mensagem para: " + mensagem.getDestinatario());
+                    }
+                });
+
         editor = new Editor(null, "Alex", "alex@gmail.com", BigDecimal.TEN, true);
     }
 
@@ -33,5 +34,34 @@ class CadastroEditorTest {
     public void Dado_um_editor_valido_quando_criar_entao_deve_retornar_id_de_cadastro() {
         Editor editorCriado = cadastroEditor.criar(editor);
         Assertions.assertEquals(1L, editorCriado.getId());
+        Assertions.assertTrue(armazenamentoEditor.here);
+    }
+
+    @Test
+    public void Dado_um_editor_null_quando_criar_entao_deve_retornar_exception() {
+
+        Assertions.assertThrows(NullPointerException.class, ()-> {
+            Editor editorCriado = cadastroEditor.criar(null);
+        });
+
+        Assertions.assertFalse(armazenamentoEditor.here);
+    }
+
+    @Test
+    public void Dado_um_editor_com_email_existente_quando_criar_entao_deve_lancar_exception() {
+
+        Assertions.assertThrows(RegraNegocioException.class, ()-> {
+            Editor editorCriado = cadastroEditor.criar(editor);
+        });
+    }
+
+    @Test
+    public void Dado_um_editor_com_email_existente_quando_criar_entao_nao_deve_salvar() {
+
+        try {
+            Editor editorCriado = cadastroEditor.criar(editor);
+        } catch (RegraNegocioException e) {
+            System.out.println(e.getMessage());
+        }
     }
 }
--- src/test/java/com/algaworks/junit/blog/negocio/stub/ArmazenamentoEditorFixoEmMemoria.java
+++ src/test/java/com/algaworks/junit/blog/negocio/stub/ArmazenamentoEditorFixoEmMemoria.java
@@ -1,17 +1,21 @@
-package com.algaworks.junit.blog.negocio;
+package com.algaworks.junit.blog.negocio.stub;
 
 import com.algaworks.junit.blog.armazenamento.ArmazenamentoEditor;
 import com.algaworks.junit.blog.modelo.Editor;
 
+import java.math.BigDecimal;
 import java.util.List;
 import java.util.Optional;
 
 public class ArmazenamentoEditorFixoEmMemoria implements ArmazenamentoEditor {
+
+    public boolean here = false;
     @Override
     public Editor salvar(Editor editor) {
         if (editor.getId() == null) {
             editor.setId(1L);
         }
+        here = true;
         return editor;
     }
 
@@ -22,6 +26,9 @@ public class ArmazenamentoEditorFixoEmMemoria implements ArmazenamentoEditor {
 
     @Override
     public Optional<Editor> encontrarPorEmail(String email) {
+        if (email.equals("alex@gmail.com")) {
+            return Optional.of(new Editor(1L, "Alex", "alex@gmail.com", BigDecimal.TEN, true));
+        }
         return Optional.empty();
     }
 
